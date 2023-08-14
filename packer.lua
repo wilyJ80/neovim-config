81f8d4ee0114cf41ec61e3b4d0643e9779c55a6e
Victor Hugo <136921549+wilyJ80@users.noreply.github.com>
2023-08-14 21:44:54 +0000

    Add files via upload

---
+++ packer.lua
@@ -0,0 +1,33 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
+
+	use 'tanvirtin/monokai.nvim'
+
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+		}
+	}
+end)
---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#install my software
+sudo apt update
+sudo apt install -y git xclip build-essential openjdk-17-jdk maven python3 curl wget unzip tar gzip
+
+#copy files needed
+mkdir ~/.config/nvim
+cd ~/
+cp -f init.lua ~/.config/nvim
+cp -f packer.lua ~/.config/nvim/
+
+#install neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+#./nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+# Optional: exposing nvim globally.
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+#nvim
+
+#install packer manager
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+#lets configure
+cd ~/.config/nvim
+nvim
