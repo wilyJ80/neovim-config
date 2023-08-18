14923a36f70cf66d37def410b7ef74bf7c2706fe
Victor Hugo <136921549+wilyJ80@users.noreply.github.com>
2023-08-18 15:58:06 -0300

    Update and rename test.sh to script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-#install my software
+# install my software
 sudo apt update
 sudo apt install -y git xclip build-essential openjdk-17-jdk maven python3 curl wget unzip tar gzip
 
-#copy files needed
-mkdir ~/.config/nvim
-cd ~/neovim-config/
-cp -f init.lua ~/.config/nvim
-cp -f packer.lua ~/.config/nvim/
-
-#install neovim
+# install neovim (appimage)
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 #./nvim.appimage
@@ -21,10 +15,10 @@ sudo mv squashfs-root /
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 #nvim
 
-#install packer manager
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# Add my config based on Kickstart.nvim
+mkdir ~/.config/nvim
+git clone https://github.com/wilyJ80/kickstart.nvim ~/.config/nvim
 
-#lets configure
+# lets configure
 cd ~/.config/nvim
 nvim
